f3d337a1d666dbcd5751eaf9c8e7515640b5a843
quocpt <thanhquocuit@gmail.com>
2024-03-23 15:56:58 -0700

    init frontend project with React

---
+++ frontend-react/src/setupProxy.js
@@ -0,0 +1,43 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  /*
+  app.use(
+    createProxyMiddleware("/api/ws", {
+      target: "http://localhost:5000",
+      changeOrigin: true,
+      ws: true,
+    })
+  );
+  app.use(
+    "/api/*",
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    "//",
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true,
+    })
+  );
+  app.use(function (req, res, next) {
+    switch (req.path) {
+      case "/unity/chess3dunity.data.gz":
+      case "/unity/chess3dunity.framework.js.gz":
+        res.setHeader("Content-Encoding", "gzip");
+        break;
+      case "/unity/chess3dunity.wasm.gz":
+        res.setHeader("Content-Encoding", "gzip");
+        res.setHeader("Content-Type", "application/wasm");
+        break;
+      default:
+        break;
+    }
+
+    next();
+  });
+  */
+};
