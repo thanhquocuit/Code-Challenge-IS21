2c8ae12de7ecbd2c353386f37728af5db0909248
quocpt <thanhquocuit@gmail.com>
2024-03-23 23:29:48 -0700

    add kanpan board component

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,15 +1,82 @@
+import { Box, Card, CardBody, CardHeader, Flex, Heading, Icon, Text } from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
-import { Container } from 'react-bootstrap';
-import { Heading } from '@chakra-ui/react';
+import { GrDrag } from "react-icons/gr";
+import { ReactSortable } from "react-sortablejs";
 import PageTemplate from './component/PageTemplate';
 
+
+function Note(props: { color: string }) {
+  return (
+    <Card mb='5' boxShadow='4px 4px 5px 2px rgba(0,0,0,0.2)' attr-color={props.color}>
+      <CardHeader className='card-handle' display='flex'>
+        <Box flex='0' minWidth='40px'>
+          <Icon w={5} h={5} as={GrDrag} />
+        </Box>
+        <Heading flex='2' size='md'>{props.color}</Heading>
+      </CardHeader>
+      <CardBody>
+        <Flex justifyContent='space-between'>
+          <Text>{props.color}</Text>
+          <Box width='32px' bgColor={props.color}></Box>
+        </Flex>
+      </CardBody>
+    </Card>
+  )
+}
+
+function KanbanColumn(props: { title: string, items: any[], listGroup: string }) {
+  const [state, setState] = React.useState<any[]>(props.items);
+
+  return (
+    <Box w='100%' h='100%'>
+      <Text>{props.title}</Text>
+      <ReactSortable className='board-cont' list={state} setList={setState} handle='.card-handle' animation={200}
+        group={props.listGroup} ghostClass='ghost' chosenClass='chosen' onAdd={(evt) => {
+          console.log(`${props.title}: ${evt.item.getAttribute('attr-color')}`)
+        }}>
+        {state.map((item) => (
+          <Note key={item.id} color={item.color} />
+        ))}
+      </ReactSortable>
+    </Box>
+  );
+}
+
+function KanbanBoard(props: { title: string }) {
+
+  const listGroup = props.title.replaceAll(/\s+/g, '')
+  return (
+    <Box w='100%'>
+      <Heading size='lg'>Sorted by {props.title}</Heading>
+      <Flex className='kanban-board' dir='row' gap='5'>
+        <KanbanColumn title='Available' listGroup={listGroup} items={[
+          { id: 1, color: "blue" },
+          { id: 2, color: "grey" },
+          { id: 3, color: "white" },
+        ]} />
+
+        <KanbanColumn title='Running Low' listGroup={listGroup} items={[
+          { id: 4, color: "purple" },
+        ]} />
+
+        <KanbanColumn title='Out of Stock' listGroup={listGroup} items={[
+          { id: 5, color: "black" },
+        ]} />
+      </Flex>
+    </Box>
+  )
+}
+
 function App() {
+  function Separator() {
+    return <hr style={{ width: '33%' }} />
+  }
+
   return (
     <PageTemplate>
-      <Container>
-        <Heading>Hello</Heading>
-      </Container>
+      <KanbanBoard title="Paint Stock" />
+      <Separator />
+      <KanbanBoard title="Orders" />
     </PageTemplate>
   );
 }
--- frontend-react/src/types.d.ts
+++ frontend-react/src/types.d.ts
@@ -1,3 +1,15 @@
+declare module 'ui-avatar-svg' {
+    export default class UIAvatarSvg {
+        text(txt: string): UIAvatarSvg;
+        fontFamily(val: string): UIAvatarSvg;
+        fontWeight(val: string): UIAvatarSvg;
+        size(val: number): UIAvatarSvg;
+        bgColor(color: string): UIAvatarSvg;
+        textColor(color: string): UIAvatarSvg;
+        generate(): voidd;
+    }
+}
+
 type PropsWithChildren<P> = P & { children?: ReactNode };
 
 type PropsAny = PropsWithChildren<{}>
